9e7761b9877528071bd3cb0d830e939accc1078c
Iqbal Shah Nadiri <iqbalshahnadiri786@gmail.com>
2024-02-03 16:53:46 +0500

    create expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import Form from "./Form";
+import { useState } from "react";
+import ExpenseList from "./ExpenseList";
 
 const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Buy a book", amount: 30, category: "Books" },
+    { id: 2, description: "Buy a pen", amount: 10, category: "Stationary" },
+    {
+      id: 3,
+      description: "Buy a laptop",
+      amount: 300,
+      category: "Electronics",
+    },
+  ]);
+
   return (
     <div className="p-4">
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
     </div>
   );
 };
